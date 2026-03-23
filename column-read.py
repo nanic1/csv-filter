a70b649e5a2a62a59864c48b19ed1f3b0f173bf9
Pedro Kurtz <Pedro.Kurtz@apsant.rj>
2026-03-23 09:51:23 -0300

    ADD : added some info to header, created new var for header and added comments about the code in ptbr

--- column-read.py
+++ column-read.py
@@ -11,11 +11,20 @@ key_words = ["CONDOMINIO", "EDIFICIO", "CONDOMÍNIO", "EDIFÍCIO", "COND."]
 with open(input_file, "r", encoding="latin-1", newline="") as infile, \
      open(output_file, "w", encoding="utf-8", newline="") as outfile:
     
+    # delimitadores por ";"
+    # reader = comando para ler arquivo
+    # writer = comando para escrever em arquivo
+    # QUOTE_ALL = coloca tudo entre aspas (força a celula a se tornar string)
     reader = csv.reader(infile, delimiter=";")
     writer = csv.writer(outfile, delimiter=";", quoting=csv.QUOTE_ALL)
 
-    # cabeçalho
-    writer.writerow(["CNPJ", "NOME", "ENDERECO", "BAIRRO", "UF", "TELEFONE1", "TELEFONE2", "TELEFONE3"])
+    # escreve cabeçalho no novo arquivo
+    writer.writerow([
+        "CNPJ", "MATRIZ", "NOME", "SITUAÇÃO CADASTRAL", "DATA SITUAÇÃO CADASTRAL", 
+        "NOME FANTASIA", "DATA DE INÍCIO DE ATIVIDADE", "CNAE PRINCIPAL", "CNAE SECUNDÁRIO", 
+        "ENDERECO", "BAIRRO", "CEP", "UF", "CÓDIGO MUNICÍPIO", 
+        "TELEFONE1", "TELEFONE2", "FAX", "EMAIL"
+        ]) #18
 
     print("Copiando arquivos, aguarde um momento...")
 
@@ -27,20 +36,40 @@ with open(input_file, "r", encoding="latin-1", newline="") as infile, \
         total += 1
         nome = row[4].upper()
 
+        # se o nome que foi lido NÃO corresponde as palavras chaves escritas = não faça nada
         if not any(p in nome for p in key_words):
             continue
-
+        
+        # caso nome corresponda as palavras chaves = siga os comandos abaixo
+        # variaveis para organizar as informações uteis e que sejam de acordo com o cabeçalho delimitado
         cnpj = "".join(x.strip() for x in row[0:3])
-        endereco = " ".join(x.strip() for x in row[13:16]).upper()
+        matriz = row[3]
+        situ_cadastral = row[5]
+        data_situ_cadatral = row[6]
+        nome_fantasia = row[8].upper()
+        data_abertura = row[10]
+        cnae1 = row[11]
+        cnae2 = row[12]
+        endereco = " ".join(x.strip() for x in row[13:17]).upper()
         bairro = row[17].upper()
+        cep = row[18]
         uf = row[19].upper()
+        municipioIBGE = row[20]
         tel1 = "".join(x.strip() for x in row[21:23])
         tel2 = "".join(x.strip() for x in row[23:25])
-        tel3 = "".join(x.strip() for x in row[25:27])
+        fax = "".join(x.strip() for x in row[25:27])
+        email = row[27].lower()
+        #18
         
         filtrado += 1
-        writer.writerow([cnpj, nome, endereco, bairro, uf, tel1, tel2, tel3])
 
+        # escrever dados abaixo do cabeçalho na ordem informada
+        writer.writerow([
+            cnpj, matriz, nome, situ_cadastral, data_situ_cadatral, nome_fantasia, data_abertura, 
+            cnae1, cnae2, endereco, bairro, cep, uf, municipioIBGE, tel1, tel2, fax, email
+        ]) #18
+
+# prints para saber se o arquivo informado foi lido e se foram escritas linhas de acordo com o filtro
 print("Arquivos copiados com sucesso!")
 print(f"Total de linhas: {total}")
 print(f"Linhas filtradas: {filtrado}")
